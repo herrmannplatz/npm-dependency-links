215c3af3f45ef70b11c23f66ff3970ca110a2f82
herrmannplatz <rene@herrmannplatz.de>
2017-11-09 21:42:03 +0100

    Create links for packages only once

--- extension.js
+++ extension.js
@@ -1,41 +1,41 @@
 const { languages, Uri, DocumentLink, Range } = require('vscode');
 const path = require('path');
 
+function buildLink(line, lineIndex, package) {
+    const startCharacter = line.text.indexOf(package);
+    const endCaracter = startCharacter + package.length;
+    const linkRange = new Range(lineIndex, startCharacter, lineIndex, endCaracter);
+    const linkUri = Uri.parse(`https://www.npmjs.com/package/${package}`);
+    return new DocumentLink(linkRange, linkUri);
+}
+
 exports.activate = function (context) {
     const disposable = languages.registerDocumentLinkProvider(['javascript', { pattern: '**/package.json' }], {
         provideDocumentLinks(document, token) {
-            const pkg = JSON.parse(document.getText());
-            const { 
-                dependencies = {}, 
-                devDependencies = {}, 
-                optionalDependencies = {},
-                peerDependencies = {},
-            } = pkg;
-
-            const links = [];
+            let links = [];
             let lineIndex = 0;
-
-            function extractLink(line, package, version) {
-                if (line.text.match(package, version)) {
-                    const startCharacter = line.text.indexOf(package);
-                    const endCaracter = startCharacter + package.length;
-                    const linkRange = new Range(lineIndex, startCharacter, lineIndex, endCaracter);
-                    const linkUri = Uri.parse(`https://www.npmjs.com/package/${package}`);
-                    links.push(new DocumentLink(linkRange, linkUri));
-                }
-            }
+            let shouldCheckForDependency = false;
             
             while (lineIndex < document.lineCount) {
                 const line = document.lineAt(lineIndex);
 
-                [
-                    ...Object.entries(dependencies),
-                    ...Object.entries(devDependencies),
-                    ...Object.entries(optionalDependencies),
-                    ...Object.entries(peerDependencies)
-                ].forEach(([package, version]) => {
-                    extractLink(line, package, version);
-                });
+                if (shouldCheckForDependency) {
+                    // no need to check for dependencies if block ended
+                    if (line.text.includes('}')) {
+                        shouldCheckForDependency = false;
+                    } else {
+                        // find dependecy
+                        const matches = line.text.match(/"(.*?)"/);
+                        
+                        if (matches) {
+                            links.push(buildLink(line, lineIndex, matches[1]));
+                        }
+                    }
+
+                } else {
+                    // check if we are in a dependencies block
+                    shouldCheckForDependency = /"(.*?)dependencies"/i.test(line.text);
+                }
                     
                 lineIndex += 1;
             }
@@ -45,7 +45,4 @@ exports.activate = function (context) {
     });
 
     context.subscriptions.push(disposable)
-};
-
-exports.deactivate = function () {
 };
